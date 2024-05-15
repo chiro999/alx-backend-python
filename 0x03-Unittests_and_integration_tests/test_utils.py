d1a8cc374385deebc674a73b972496d91bb2b6c1
Nana Kwame <105567071+chiro999@users.noreply.github.com>
2024-05-15 08:50:46 +0000

    Create test_utils.py

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""A module for testing the utils module.
+"""
+import unittest
+from typing import Dict, Tuple, Union
+from unittest.mock import patch, Mock
+from parameterized import parameterized
+
+from utils import (
+    access_nested_map,
+    get_json,
+    memoize,
+)
